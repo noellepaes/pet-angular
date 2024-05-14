2d5617dbcc7382ce29bbb9ad216277323b770c29
noellepaes <noellepaes12345@gmail.com>
2024-05-14 09:35:55 -0300

    rota tabela e lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'tabela' },
+  {
+    path: 'tabela',
+    loadChildren: () =>
+      import('./tabela/tabela.module').then((m) => m.TabelaModule),
+  },
 ];
---
+++ src/app/tabela/tabela.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TabelaRoutingModule } from './tabela-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TabelaRoutingModule
+  ]
+})
+export class TabelaModule { }
