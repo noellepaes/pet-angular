3cf51cb81625afe26bcc33769720d3c64a1fc2bb
noellepaes <noellepaes12345@gmail.com>
2024-05-15 13:21:22 -0300

    card

--- src/app/tabela/tabela/tabela.component.ts
+++ src/app/tabela/tabela/tabela.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { Tabela } from '../model/course';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-tabela',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule],
   templateUrl: './tabela.component.html',
   styleUrl: './tabela.component.css',
 })
-export class TabelaComponent implements OnInit{
-
+export class TabelaComponent implements OnInit {
   tabela: Tabela[] = [
     {
-      _id: '1', nome: 'branca', apelido: 'suricato' }
+      _id: '1',
+      nome: 'branca',
+      apelido: 'suricato',
+    },
   ];
-  dispayedColumns = ['nome', 'apelido']
+  dispayedColumns = ['nome', 'apelido'];
 
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 }
--- src/app/tabela/tabela/tabela.module.ts
+++ src/app/tabela/tabela/tabela.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TabelaRoutingModule } from './tabela-routing.module';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    TabelaRoutingModule
-  ]
+  imports: [CommonModule, TabelaRoutingModule, MatCardModule],
 })
-export class TabelaModule { }
+export class TabelaModule {}
